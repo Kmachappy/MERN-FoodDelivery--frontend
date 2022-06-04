8a99bd59114718470202625613801a70e9f1dca5
Alejandro Rojas <kmachappy@pm.me>
2022-06-04 01:37:05 -0700

    updates to sign up

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,7 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import './Nav.css'
+
 export default function Nav(){
     return(
         <div className="navbar">
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -7,15 +7,15 @@ import axios from 'axios'
 export default function Signup(){
     const navigate = useNavigate()
     const url = "https://food-rojas.herokuapp.com/"
+    
+    const [resp, setResp] = useState(null)
     const [form, setForm] = useState({
         email:"",
         password:""
     })
-    const [resp, setResp] = useState(null)
-    // console.log(resp|| resp==="sucesss"? null:resp)
+    
     console.log(resp)
     function handleChange(event){
-        // console.log(event.target.name,event.target.value)
         setForm({
             ...form,
             [event.target.name]: event.target.value
@@ -43,12 +43,14 @@ export default function Signup(){
             {resp !== "success"? <h4>{resp}</h4>: null }
             <form onSubmit={handleSubmit}>
                 <input 
-                    type="text" 
+                    type="email" 
+                    pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
                     name="email"
                     placeholder='email'
                     value={form.email}
                     onChange={handleChange}
                     maxLength="50"
+                    required
                 />
                 <br />
                 <input 
@@ -59,6 +61,7 @@ export default function Signup(){
                     onChange={handleChange}
                     minLength="6"
                     maxLength="32"
+                    required
                 />
                 <br />
                 <button type='submit'>Sign Up</button>
